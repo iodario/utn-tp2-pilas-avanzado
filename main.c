fc55408abd15ab59cb39c0551647e241fa35a88d
Dario Turchi <iodariomdq@outlook.com>
2025-03-11 21:11:31 -0300

    cambios

--- main.c
+++ main.c
@@ -1,79 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <conio.h> // Para usar getch()
+
+// Prototipo de la función
+void opcionesMenu();
 
 int main()
 {
     char opcion;
 
-
     do
     {
-
         opcionesMenu();
+        opcion = getch(); // Captura la opción sin necesidad de presionar Enter
 
+        system("cls"); // Limpia pantalla en Windows
 
-
-        opcion = getch();
-
-        system("cls");
-
-    switch(opcion)
+        switch(opcion)
         {
-        case 27:
-            printf("Bye Bye !\n");
-
-            break;
-        case 49:
-
-            codigoAscii();
-            break;
-        case 50:
-            printf("\n\n\t\t\t Archivos de la carpeta actual");
-            system("dir");
-
-
-            }
-
-    system("pause");
-    system("cls");
-   }while (opcion !=27);
-
-
-
-return 0;
-}
-// suma
-
-void opcionesMenu() {
-        printf("\n\t\t\t Menu de opciones \n");
-        printf("\n 1 - Tabla ASCII \n");
-        printf("\n 2 - Ejercicio 1 \n");
-        printf("\n 3 - Ejercicio 2 \n");
-        printf("\n 4 - Ejercicio 3 \n");
-        printf("\n 5 - Ejercicio 4 \n");
-        printf("\n 6 - Ejercicio 5 \n");
-        printf("\n 7 - Ejercicio 6 \n");
-        printf("\n 8 - Ejercicio 7 \n\n");
-        printf("\n Ingrese una opcion, o ESC para salir ...\n");
-}
-
-int suma(int uno, int dos){
- return uno + dos;
-}
-
-void codigoAscii(){
-printf("\n\n\t\t-------Tabla de caracteres ASCII -------\n");
-            for (int i=0; i<256 ; i++)
-            {
-                printf("Nro: %d - %c |", i, i);   //i como entero, y luego i como caracter
-                if(i%5==0)
-                {
-                printf("\n");
-            }
+            case '1':
+                printf("\n 1 - Ejercicio 1 \n\n");
+                printf("\n 1.- Sumar los elementos de una pila (usar variables enteras) \n\n");
+                break;
+            case '2':
+                printf("\n 2 - Ejercicio 2 \n\n");
+                printf("\n 2.- Contar los elementos de una pila (usar variables enteras) \n\n");
+                break;
+            case '3':
+                printf("\n 3 - Ejercicio 3 \n\n");
+                printf("\n 3.- Calcular el promedio de los valores de una pila (usar variables) \n\n");
+                break;
+            case '4':
+                printf("\n 4 - Ejercicio 4 \n\n");
+                printf("\n 4.- Determinar si un elemento buscado está dentro de una pila. \n\n");
+                break;
+            case '5':
+                printf("\n 5 - Ejercicio 5 \n\n");
+                printf("\n 5.- Eliminar un elemento de una pila y mantener el resto en el mismo orden. \n\n");
+                break;
+            case '6':
+                printf("\n 6 - Ejercicio 6 \n\n");
+                printf("\n 6.- Verificar si una pila dada es capicúa. \n\n");
+                break;
+            case '7':
+                printf("\n 7 - Ejercicio 7 \n\n");
+                printf("\n 7.- Calcular la unión de dos pilas A y B que simulan conjuntos. \n\n");
+                break;
+            case '8':
+                printf("\n 8 - Ejercicio 8 \n\n");
+                printf("\n 8.- Sumar los elementos de una pila (usar variables enteras). \n\n");
+                break;
+            case '9':
+                printf("\n 9 - Ejercicio 9 \n\n");
+                printf("\n 9.- Sumar los elementos de una pila (usar variables enteras). \n\n");
+                break;
+            case 27: // Código ASCII de la tecla ESC
+                printf("Bye Bye!\n");
+                break;
+            default:
+                printf("\n Opción no válida. Intente de nuevo.\n");
         }
+
+        printf("\nPresione cualquier tecla para continuar...\n");
+        getch(); // Espera que el usuario presione una tecla para continuar
+        system("cls"); // Limpia pantalla en Windows
+
+    } while (opcion != 27);
+
+    return 0;
 }
+
+void opcionesMenu()
+{
+    printf("\n\t\t\t Trabajo Práctico 2 - Pilas Avanzado \n");
+    printf("\n 1 - Ejercicio 1 \n");
+    printf(" 2 - Ejercicio 2 \n");
+    printf(" 3 - Ejercicio 3 \n");
+    printf(" 4 - Ejercicio 4 \n");
+    printf(" 5 - Ejercicio 5 \n");
+    printf(" 6 - Ejercicio 6 \n");
+    printf(" 7 - Ejercicio 7 \n");
+    printf(" 8 - Ejercicio 8 \n");
+    printf(" 9 - Ejercicio 9 \n");
+    printf("\n Ingrese una opción, o presione ESC para salir...\n");
+}
+
 
 
 
